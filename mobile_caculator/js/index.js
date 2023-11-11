316fe8469b222e865aa2e76d6dd6455b469d1366
JacksonLinQAQ <83115037+JacksonLinQAQ@users.noreply.github.com>
2023-11-11 13:00:35 +0800

    basic js
    use eval implement the calculator function

--- mobile_caculator/js/index.js
+++ mobile_caculator/js/index.js
@@ -1,7 +1,32 @@
 window.addEventListener('load', ()=>{
     var expression = ''
-    document.querySelectorAll('button')
+    var screen = document.querySelector('.screen')
+    document.querySelectorAll('button:not(.operator, .three)')
+    .forEach((button)=>{
+        button.addEventListener('click', (e)=>{
+            expression += e.target.innerText
+            let arr = expression.replaceAll('+', ' ')
+            .replaceAll('-', ' ')
+            .replaceAll('*', ' ')
+            .replaceAll('/', ' ')
+            .split(' ')
+            screen.innerText = arr[arr.length - 1]
+        })
+    })
+    document.querySelectorAll('button.operator:not(#eq)')
+    .forEach((button)=>{
+        button.addEventListener('click', (e)=>{
+            expression += e.target.innerText
+        })
+    })
+    document.querySelector('button#ac')
     .addEventListener('click', (e)=>{
-        
+        expression = ''
+        screen.innerText = '0'
+    })
+    document.querySelector('button#eq')
+    .addEventListener('click', (e)=>{
+        console.log(expression)
+        screen.innerText = eval(expression)
     })
 })
