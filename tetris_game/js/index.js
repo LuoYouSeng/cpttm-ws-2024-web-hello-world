cac265e59ba7956be7918ba31903ede16d257385
SHAOKP <3316211327@qq.com>
2023-12-09 13:01:09 +0800

    改用class寫俄羅斯方塊

--- tetris_game/js/index.js
+++ tetris_game/js/index.js
@@ -1,67 +1,151 @@
 const playground = document.querySelector('#playground')
-const input = document.querySelector('#input')
 
-var map = new Array(20)
-for(let i = 0; i < 20;i ++) {
-    map[i] = new Array(10).fill(0)
+class Shape {
+    get rows() {
+        return this.shape.length;
+    }
+    get cols() {
+        return this.shape[0].length;
+    }
+    rotate() {
+        const nArr = Array.from({
+            length: this.cols
+        }, () => []);
+
+        this.shape.forEach((row, i) => {
+            row.forEach((val, j) => {
+                nArr[j][this.rows - i - 1] = val;
+            });
+        });
+
+        this.shape = nArr;
+    }
 }
 
-var blocks = {
-    I: [
-        {
-            shape: [
-                [1],
-                [1],
-                [1],
-                [1]
-            ],
-            offset: {x: 0, y: -4},
-            isOverlap: function(map) {
-                return map[y + 1][x] === 1
-            }
-        }, {
-            shape: [
-                [1, 1, 1, 1]
-            ],
-            offset: {x: -1, y: -1},
-            isOverlap: function(map) {
-                return map[y + 1][x] === 1 || 
-                    map[y + 1][x + 1] === 1 || 
-                    map[y + 1][x + 2] === 1 || 
-                    map[y + 1][x + 3] === 1
-            }
+class IShape extends Shape {
+    constructor() {
+        super()
+        this.shape = [
+            [1, 1, 1, 1]
+        ]
+    }
+}
+
+class LShape extends Shape {
+    constructor() {
+        super()
+        this.shape = [
+            [1, 1, 1],
+            [1, 0, 0]
+        ]
+    }
+}
+
+class JShape extends Shape {
+    constructor() {
+        super()
+        this.shape = [
+            [1, 0, 0],
+            [1, 1, 1]
+        ]
+    }
+}
+
+class OShape extends Shape {
+    constructor() {
+        super()
+        this.shape = [
+            [1, 1],
+            [1, 1]
+        ]
+    }
+}
+
+class ZShape extends Shape {
+    constructor() {
+        super()
+        this.shape = [
+            [1, 1, 0],
+            [0, 1, 1]
+        ]
+    }
+}
+
+class SShape extends Shape {
+    constructor() {
+        super()
+        this.shape = [
+            [0, 1, 1],
+            [1, 1, 0]
+        ]
+    }
+}
+
+var shapes = new Array()
+
+shapes.push(new IShape())
+shapes.push(new LShape())
+shapes.push(new JShape())
+shapes.push(new OShape())
+shapes.push(new ZShape())
+shapes.push(new SShape())
+  
+class Game {
+    constructor() {
+        this.map = new Array(20)
+        this.y = 0
+        this.x = 0
+        this.shape = this.randomShape()
+        for(let i = 0; i < 20;i ++) {
+            this.map[i] = new Array(10).fill(0)
         }
-    ]
-}
+    }
 
-var x = 4, y = 0, rotation = 1, block = blocks.I
+    randomShape() {
+        return shapes[Math.floor(Math.random() * shapes.length)]
+    }
 
-function drawBlock() {
-    let rb = block[rotation % block.length]
-    rb.shape.forEach((row, by) => {
-        row.forEach((col, bx) => {
-            map[y + by + rb.offset.y] = 1
-            map[x + bx + rb.offset.x] = 1
+    renderBlock(x, y, shape) {
+        shape.forEach((row, sy) => {
+            row.forEach((col, sx) => {
+                if(col == 1) {
+                    let block = document.createElement('div')
+                    block.classList = 'block'
+                    block.style.top = (y + sy) * 40 + 'px'
+                    block.style.left = (x + sx) * 40 + 'px'
+                    playground.appendChild(block)
+                }
+            })
         })
-    })
+    }
+
+    render() {
+        playground.innerHTML = ''
+        this.renderBlock(this.x, this.y, this.shape.shape)
+        this.map.forEach((row, fy) => {
+            row.forEach((col, fx) => {
+                if(col == 1) {
+                    this.renderBlock(fx, fy, [1])
+                }
+            })
+        })
+    }
+    
+    loop() {
+        this.render()
+        this.y += 1
+    }
 }
 
-function render(playground) {
-    map.forEach((row, y) => {
-        row.forEach((col, x) => {
-            if(col === 1) {
-                let div = document.createElement('div')
-                div.style.top = y * 40 + 'px'
-                div.style.left = x * 40 + 'px'
-                div.classList.add('block')
-                playground.appendChild(div)
-            }
-        })
-    })
-}
+var game = new Game()
+
+document.addEventListener('keydown', (e) => {
+    switch(e.code) {
+        case 'KeyD':
+            game.x += 1
+    }
+})
 
 setInterval(() => {
-    drawBlock()
-    render(playground)
-    y += 1
+    game.loop()
 }, 1000)
