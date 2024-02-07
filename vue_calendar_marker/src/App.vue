9681605cf3fe2c9e6c87585337f0cf564513a5b3
SHAOKP <3316211327@qq.com>
2024-02-07 23:32:37 +0800

    merge and remove
    merge front practices, remove vue todo list and vue calendar marker

--- vue_calendar_marker/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-    import moment from 'moment'
-</script>
-
-<template>
-    <div>{{ moment('2024W046').format() }}</div>
-</template>
--- vue_todos/src/models/todo.js
+++
@@ -1,9 +0,0 @@
-import moment from "moment"
-
-export default class Todo {
-    constructor() {
-        this.task = ''
-        this.date = moment().format('yyyy-MM-DD')
-        this.isDone = false
-    }
-}
